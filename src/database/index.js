4eaccf2b1c3832e5eed6737cde3ffa23ffb20d63
Rafael <rafael@Rafaels-MacBook-Pro.local>
2019-12-15 22:04:08 -0300

    Added import

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize'); //efetuando o import do sequelize para utlização
 const dbConfig = require('../config/database');//efetuando o import da configuração de comunicação com o banco de dados
+const Cliente = require('../models/Cliente')
+const Academia = require('../models/Academia')
 
 //criando a conexão com o banco de dados - chamando o Sequelize que foi importado e jagando o JSON da config/database.js
 const connection =  new Sequelize(dbConfig);
