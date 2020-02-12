3362c92f84699a1698e60fd582ee502f3e198e62
Rafael <macedo.rafanazareth@gmail.com>
2020-02-12 15:22:45 -0300

    Começando a desenvolver a funcionalidade para devolver a imagem no formato base64

--- src/controller/ImagemController.js
+++ src/controller/ImagemController.js
@@ -1,4 +1,5 @@
 const Imagem = require("../models/Imagem")
+const fs = require("fs")
 
 module.exports = {
 
@@ -6,8 +7,14 @@ module.exports = {
         const { file, extension, fileName } = req.body
         const _file = Buffer.from(file, 'utf8')
         await Imagem.create({ image: _file, extension, fileName })
-
-
+            .then((image) => {
+                const file = fs.writeFile("teste.jpg", image.file, (error) => { console.log(error) })
+                const buff = fs.read
+                const base64Data = buff.toString("base64");
+                res.json(base64Data)
+            }).catch((error) => {
+                console.log(error)
+            })
     },
 
 }
